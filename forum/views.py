329349d59ae5f8a0647a592513d8426906cfdba8
mardea-s <mardeajs@yahoo.com>
2017-08-14 15:44:13 -0400

    Added adding a new blog post.

--- forum/views.py
+++ forum/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -43,3 +44,15 @@ def hotlines(request):
 
 def references(request):
     return render(request, 'forum/references.html', {})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+    else:
+        form = PostForm()
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.published_date = timezone.now()
+        post.save()
+    return render(request, 'forum/post_edit.html', {'form': form})
